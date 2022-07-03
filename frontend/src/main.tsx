c55556c776a9e10effd481e13a757e06da87e93f
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-07-03 18:58:06 +0300

    feat: add base styles for the `frontend` to pass linting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Routing } from '@navigation/routing/routing';
+import '@assets/css/styles.scss';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);

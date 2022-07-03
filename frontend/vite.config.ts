bf2c990d0462473db21f3605433c58c9b10d38e1
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-07-03 11:57:56 +0300

    chore: add paths aliases for root folders

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,25 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@common': path.resolve(__dirname, './src/common'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@helpers': path.resolve(__dirname, './src/helpers'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@validation-schemas': path.resolve(
+        __dirname,
+        './src/validation-schemas',
+      ),
+      '@navigation': path.resolve(__dirname, './src/navigation'),
+    },
+  },
 });

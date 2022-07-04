7574d33396ceb85f13f8ba59791a8bafa19825ec
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-07-04 22:50:08 +0300

    feat: add basic health controller

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,16 @@
-// eslint-disable-next-line no-console
-console.log(process.env.FRONTEND_URL);
+import cors from 'cors';
+import express from 'express';
 
-export {};
+// Express server configuration
+const app = express();
+app
+  .use(cors({ origin: '*' })) // TODO Replace with the frontend url
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }));
+
+app.get('/health', async (_, res) => res.json({ ok: true }));
+
+app.listen(3001, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running at 3001');
+});

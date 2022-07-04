d04777e6536e8a8bfad69f9cd0e59e2830d15b76
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-07-04 22:57:51 +0300

    feat: replace hardcoded values with ENV

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,22 @@
+import { ENV } from '@common/enums/app/env.enum';
 import cors from 'cors';
 import express from 'express';
 
 // Express server configuration
 const app = express();
 app
-  .use(cors({ origin: '*' })) // TODO Replace with the frontend url
+  .use(
+    cors({
+      // Allow everything for dev env to test from the Postman or other API dev tools
+      origin: ENV.APP.NODE_ENV === 'development' ? '*' : ENV.APP.FRONTEND_URL,
+    }),
+  )
   .use(express.json())
   .use(express.urlencoded({ extended: true }));
 
 app.get('/health', async (_, res) => res.json({ ok: true }));
 
-app.listen(3001, () => {
+app.listen(ENV.APP.SERVER_PORT, ENV.APP.SERVER_HOST, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is running at 3001');
+  console.log(`Server is running at ${ENV.APP.SERVER_PORT}`);
 });

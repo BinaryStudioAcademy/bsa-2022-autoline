225e33e0ac59dd65bd6e63dbd3640bd359d69a20
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-07-11 21:56:24 +0300

    fix!: remove `Converting circular structure to JSON` exceptions

--- backend/src/middlewares/exceptions/exceptions.middleware.ts
+++ backend/src/middlewares/exceptions/exceptions.middleware.ts
@@ -1,15 +1,22 @@
-import { Request, Response } from 'express';
 import httpStatus from 'http-status-codes';
+
+import type { NextFunction, Request, Response } from 'express';
 
 export const errorsHandler = (
   error: Error,
   req: Request,
   res: Response,
+  next: NextFunction,
 ): void => {
   console.error(JSON.stringify(error), error.message);
-  res.status(httpStatus.BAD_REQUEST).json({
-    error,
-    message: error.message,
-  });
+  if (res.headersSent) {
+    return next(error);
+  }
+  res
+    .json({
+      error,
+      message: error.message,
+    })
+    .status(httpStatus.BAD_REQUEST);
   return;
 };

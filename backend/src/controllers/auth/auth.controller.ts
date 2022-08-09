d450d8e787375fd3cfbf973cbcc595ce78edefbe
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-08-09 11:14:09 +0300

    created signinlocal method in auth controller and added all middlewares/controller to signin route

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Prisma } from '@prisma/client';
 import * as authService from '@services/auth/auth.service';
 import httpStatus from 'http-status-codes';
 
-import type { AuthResponseDto } from '@autoline/shared';
+import type { AuthResponseDto, SignInData } from '@autoline/shared';
 import type { TypedRequestBody } from '@common/types/controller/controller';
 import type { NextFunction, Response } from 'express';
 
@@ -21,4 +21,19 @@ const signupLocal = async (
   }
 };
 
-export { signupLocal };
+const signinLocal = async (
+  req: TypedRequestBody<SignInData>,
+  res: Response<{ accessToken: string }>,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const user = req.body;
+    const loginResponseDto = authService.signinLocal(user);
+    res.json(loginResponseDto).status(httpStatus.OK);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+export { signupLocal, signinLocal };
--- backend/src/routes/auth/auth.router.ts
+++ backend/src/routes/auth/auth.router.ts
@@ -1,4 +1,5 @@
 import * as authController from '@controllers/auth/auth.controller';
+import { errorsHandler, localAuth } from '@middlewares/middlewares';
 import { Router } from 'express';
 
 const PATH = '/auth';
@@ -6,5 +7,12 @@ const PATH = '/auth';
 const authRouter = Router();
 
 authRouter.post(`${PATH}/local/signup`, authController.signupLocal);
+
+authRouter.post(
+  `${PATH}/local/signin`,
+  localAuth,
+  authController.signinLocal,
+  errorsHandler,
+);
 
 export { authRouter };

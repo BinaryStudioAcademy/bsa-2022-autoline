74cde9ff9888382b15412978731c667a12c7625c
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-09 21:03:33 +0300

    Created scrollable slider with basic style

--- frontend/src/components/cars-categories/cars-categories.tsx
+++ frontend/src/components/cars-categories/cars-categories.tsx
@@ -2,24 +2,28 @@ import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { mockBrands } from '@components/cars-categories/mock-brands';
-import { Navigation } from 'swiper';
+import { Mousewheel, Navigation, SwiperOptions } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
 
 import { Brand } from '../../types/brand.type';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/mousewheel';
 import styles from './styles.module.scss';
 
 const CarsCategories: FC = () => {
   const [brands] = useState<Brand[]>(mockBrands);
 
+  const swiperParams: SwiperOptions = {
+    slidesPerView: 8,
+    loop: true,
+    mousewheel: true,
+    navigation: true,
+    modules: [Navigation, Mousewheel],
+  };
+
   return (
-    <Swiper
-      className={styles.container}
-      modules={[Navigation]}
-      slidesPerView={6}
-      navigation={true}
-    >
+    <Swiper className={styles.container} {...swiperParams}>
       {brands.map((brand) => (
         <SwiperSlide key={brand.id}>
           <Link to="#" className={styles.navLink}>

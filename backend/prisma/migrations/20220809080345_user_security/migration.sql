ae44d8ec26cacfad3188c857d542fefdddd255f5
setar92 <setarasiuk@gmail.com>
2022-08-09 11:04:54 +0300

    added migrate for user_security

---
+++ backend/prisma/migrations/20220809080345_user_security/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `User_Security` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(300) NOT NULL,
+    `password` VARCHAR(300) NOT NULL,
+    `password_change_token` VARCHAR(300) NOT NULL,
+    `email_activation_token` VARCHAR(300) NOT NULL,
+    `email_change_token` VARCHAR(300) NOT NULL,
+    `email_provisional` VARCHAR(300) NOT NULL,
+    `refresh_token` VARCHAR(300) NOT NULL,
+    `google_acc_id` VARCHAR(300) NULL,
+    `facebook_acc_id` VARCHAR(300) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User_Security` ADD CONSTRAINT `User_Security_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

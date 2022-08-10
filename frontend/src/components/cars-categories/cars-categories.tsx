dd3c98081106c5c485b2c0b9d85f393bcc848a77
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-10 20:59:28 +0300

    fix/Customized slider nav buttons

--- frontend/src/components/cars-categories/cars-categories.tsx
+++ frontend/src/components/cars-categories/cars-categories.tsx
@@ -2,40 +2,39 @@ import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { mockBrands } from '@components/cars-categories/mock-brands';
-import { Mousewheel, Navigation, SwiperOptions } from 'swiper';
+import { SliderNavButton } from '@components/cars-categories/slider-nav-button/slider-nav-button';
+import { swiperParams } from '@components/cars-categories/slider-nav-button/swiper-params';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { Brand } from '../../types/brand.type';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/mousewheel';
+
+import { Brand } from '../../types/brand.type';
 import styles from './styles.module.scss';
 
 const CarsCategories: FC = () => {
   const [brands] = useState<Brand[]>(mockBrands);
 
-  const swiperParams: SwiperOptions = {
-    slidesPerView: 8,
-    loop: true,
-    mousewheel: true,
-    navigation: true,
-    modules: [Navigation, Mousewheel],
-  };
-
   return (
-    <Swiper className={styles.container} {...swiperParams}>
-      {brands.map((brand) => (
-        <SwiperSlide key={brand.id}>
-          <Link to="#" className={styles.navLink}>
-            <img
-              className={styles.logo}
-              src={brand.logo_url}
-              alt={brand.name}
-            />
-          </Link>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className={styles.container}>
+      <Swiper className={styles.swiperWrapper} {...swiperParams}>
+        <SliderNavButton direction="prev" />
+        <SliderNavButton direction="next" />
+
+        {brands.map((brand) => (
+          <SwiperSlide key={brand.id}>
+            <Link to="#" className={styles.navLink}>
+              <img
+                className={styles.logo}
+                src={brand.logo_url}
+                alt={brand.name}
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 

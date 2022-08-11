b75eca1859b849986585f08a2ce3105a04ccb3f7
R0ixy <nikita6kom@gmail.com>
2022-08-11 18:39:17 +0300

    add NOT_ENOUGH_PERMISSIONS to ExeptionMessage enum

--- backend/src/common/enums/exception/exception-message.enum.ts
+++ backend/src/common/enums/exception/exception-message.enum.ts
@@ -3,6 +3,8 @@ const ExceptionMessage = {
   UNAUTHORIZED_USER: 'Unauthorized user.',
   INCORRECT_CREDENTIALS:
     'Your authentication information is incorrect. Please try again.',
+  NOT_ENOUGH_PERMISSIONS:
+    'You do not have enough permission to perform this action.',
 } as const;
 
 export { ExceptionMessage };

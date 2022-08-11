9336e394c43335be3300beffe4315bc1a9784831
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-11 23:43:24 +0300

    Created data fetcher from AutoRIA API

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
+import { BrandDTO } from '../src/dtos/cars/brand.dto';
+import { PlainData } from '../src/dtos/cars/plain-data.dto';
+import { brands, manufacturers } from './seeds/fetched-data/cars-data';
 import { users } from './seeds/users';
 import { users_security } from './seeds/users-security';
 
@@ -16,8 +19,23 @@ async function main(): Promise<void> {
       data: user_security,
     });
   }
+
+  for (const manufacturer_country of manufacturers) {
+    const dataToSeed = new PlainData(manufacturer_country);
+    await prisma.manufacturer_Country.create({
+      data: dataToSeed,
+    });
+  }
+
+  for (const brand of brands) {
+    const dataToSeed = new BrandDTO(brand);
+    await prisma.brand.create({
+      data: dataToSeed,
+    });
+  }
 }
 
 main().catch((e): void => {
+  // eslint-disable-next-line no-console
   console.log(e);
 });

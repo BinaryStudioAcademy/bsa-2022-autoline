16e85496f3bcb1bb1fda995e654fdc8fd24dc8b3
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2022-08-15 22:33:27 +0300

    Fixed interface names and used mui lib

---
+++ frontend/src/common/theme/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ffffff',
+      main: '#3765ff',
+      dark: '#070a0c',
+      contrastText: '#f1f4f6',
+    },
+  },
+});

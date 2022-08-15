a8198545cac785cba02e741d8a8a6444ca272577
brovarenko <brovwg@gmail.com>
2022-08-15 10:22:16 +0300

    added query to sign in

--- frontend/src/components/sign/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/components/sign/components/sign-in-form/sign-in-form.tsx
@@ -6,15 +6,23 @@ import { ButtonFill } from '@components/common/button-fill/button-fill';
 import { ButtonOutline } from '@components/common/button-outline/button-outline';
 import { InputField } from '@components/common/input-field/input-field';
 import Divider from '@mui/material/Divider';
+import { useSignInMutation } from '@store/queries/auth';
 
 import styles from './styles.module.scss';
 
 export const SignInForm = (): React.ReactElement => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [signIn, { isLoading }] = useSignInMutation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleSubmit = (): void => {
-    setIsLoading(true);
+    const user = {
+      user: {
+        email,
+        password,
+      },
+    };
+    signIn(user).unwrap();
   };
-
   return (
     <>
       <h1 className={styles.title}>Sign In</h1>
@@ -26,12 +34,19 @@ export const SignInForm = (): React.ReactElement => {
       </p>
       <form name="signinForm" onSubmit={handleSubmit} className={styles.form}>
         <fieldset disabled={isLoading} className={styles.fieldset}>
-          <InputField name="Email" type="email" required={true} errors={''} />
+          <InputField
+            name="Email"
+            type="email"
+            required={true}
+            errors={''}
+            onChange={(event): void => setEmail(event.target.value)}
+          />
           <InputField
             name="Password"
             type="password"
             required={true}
             errors={'Must be at least 8 characters'}
+            onChange={(event): void => setPassword(event.target.value)}
           />
           <ButtonFill text="Sign In" />
         </fieldset>

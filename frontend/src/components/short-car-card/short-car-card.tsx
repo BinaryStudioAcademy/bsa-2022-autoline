0a5ae98d7d528ca7f105ada3ce21220854808d61
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-08-16 15:14:16 +0300

    changed the props field to be closer to the db, rewrote props type

--- frontend/src/components/short-car-card/short-car-card.tsx
+++ frontend/src/components/short-car-card/short-car-card.tsx
@@ -1,14 +1,15 @@
-import { CarDataType } from '@common/types/card/card';
-import { ComponentType } from '@common/types/types';
+import { FC } from 'react';
+
+import { CarDataPropsType } from '@common/types/card/card';
 
 import styles from './styles.module.scss';
 
-const ShortCarCard: ComponentType<{ carData: CarDataType }> = ({ carData }) => {
-  const { src, title, price } = carData;
+const ShortCarCard: FC<CarDataPropsType> = ({ carData }) => {
+  const { photo_url, title, price } = carData;
   return (
     <div className={styles.card}>
       <div className={styles.cardImg}>
-        <img src={src} alt="car" />
+        <img src={photo_url} alt="car" />
       </div>
       <p className={styles.cardTitle}>{title}</p>
       <p className={styles.cardPrice}>{price}</p>

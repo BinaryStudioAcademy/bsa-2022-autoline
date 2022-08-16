b2336678e9b0d9752e593589028742d2f2fb53bf
setar92 <setarasiuk@gmail.com>
2022-08-16 13:48:04 +0300

    deleted success and failed routes from frontend routing

--- frontend/src/navigation/routing/routing.tsx
+++ frontend/src/navigation/routing/routing.tsx
@@ -22,28 +22,6 @@ const Routing: FC = () => {
           <Route path={AppRoute.SIGN_IN} element={<Sign />} />
           <Route path={AppRoute.SIGN_UP} element={<Sign />} />
         </Route>
-        <Route
-          path={AppRoute.MAIL_SACCESSFUL_VALIDATION}
-          element={
-            <div>
-              <h2>Validation was successful</h2>
-              <a rel="stylesheet" href={AppRoute.ROOT}>
-                Main page
-              </a>
-            </div>
-          }
-        />
-        <Route
-          path={AppRoute.MAIL_FAILED_VALIDATION}
-          element={
-            <div>
-              <h2>Validation failed</h2>
-              <a rel="stylesheet" href={AppRoute.ROOT}>
-                Main page
-              </a>
-            </div>
-          }
-        />
         <Route element={<ProtectedRoute isAllowed={!!authData} />}>
           <Route path={AppRoute.ROOT} element={<h2>Home page</h2>} />
         </Route>

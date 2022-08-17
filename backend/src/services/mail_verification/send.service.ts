fb32309aa5dba178ea4c93d3f8d45629c7598a01
setar92 <setarasiuk@gmail.com>
2022-08-17 21:05:02 +0300

    conflict resolving

--- backend/src/services/mail_verification/send.service.ts
+++
@@ -1,8 +0,0 @@
-const mailSend = (email: string): string => {
-  // Mock token, it must be genereted here
-  const token = 'rOiBUBf2pPvOHTNBeTR8CkLGokwHADhv';
-  console.log(`Sending email to ${email}`);
-  return token;
-};
-
-export { mailSend };
--- backend/src/services/mail_verification/send_activation_link/constants.ts
+++
@@ -1,8 +0,0 @@
-const MailActivate = {
-  ACTIVATE_URL: `${process.env.VITE_API_ORIGIN_URL}/user/verification/`,
-  SUCCESS_URL: `${process.env.FRONTEND_URL}/verification-success`,
-  FAILED_URL: `${process.env.FRONTEND_URL}/verification-failed`,
-  SUBJECT: 'Welcome To AutoLine Community',
-};
-
-export { MailActivate };

2345e10152ca7305204f07ced7168330cfd3e028
setar92 <setarasiuk@gmail.com>
2022-08-17 17:03:25 +0300

    changed folder name

--- backend/src/services/auth/auth.service.ts
+++ backend/src/services/auth/auth.service.ts
@@ -3,8 +3,8 @@ import { prisma } from '@data/prisma-client';
 import { createToken } from '@helpers/helpers';
 import { bcryptHash, sendEmail } from '@helpers/helpers';
 import { User } from '@prisma/client';
-import { mailSend } from '@services/mail-verification/send.service';
-import { updateMailToken } from '@services/mail-verification/user-security.service';
+import { mailSend } from '@services/mail_verification/send.service';
+import { updateMailToken } from '@services/mail_verification/user-security.service';
 import bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 

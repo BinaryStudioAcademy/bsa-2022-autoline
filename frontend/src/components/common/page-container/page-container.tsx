60e7b8e44a06aee91137eb9e223d99d79524b400
Iryna B <iryna.burianovata@gmail.com>
2022-08-17 12:15:08 +0300

    Fix Page container width

--- frontend/src/components/common/page-container/page-container.tsx
+++ frontend/src/components/common/page-container/page-container.tsx
@@ -1,11 +1,16 @@
 import { ReactFragment } from 'react';
 
 import { Container } from '@mui/material';
+import clsx from 'clsx';
 
 import styles from './styles.module.scss';
 
 export const PageContainer = (props: {
   children: ReactFragment;
 }): React.ReactElement => {
-  return <Container className={styles.container}>{props.children}</Container>;
+  return (
+    <Container className={clsx(styles.container, styles.containerLarge)}>
+      {props.children}
+    </Container>
+  );
 };

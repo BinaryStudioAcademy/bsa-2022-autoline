76d0cd3aa5d6527adfe4dafe8524eacc4ce5eb8f
R0ixy <nikita6kom@gmail.com>
2022-08-18 11:29:06 +0300

    add city type and change 'filter' to 'find'

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -12,6 +12,7 @@ import {
   cities,
 } from '../api-autoria/cars/fetched-data/fetched-data';
 import { optionsTypes } from '../api-autoria/cars/options-types';
+import { City } from '../api-autoria/city-type';
 import { OptionType } from '../api-autoria/option-type.enum';
 import { AutoriaPlainDataDto } from '../src/dtos/cars/autoria-plain-data.dto';
 import { users } from './seeds/users';
@@ -35,11 +36,11 @@ async function main(): Promise<void> {
     const newRegion = await prisma.region.create({
       data: dataToSeed,
     });
-    const city = cities.filter(
-      (city: { data: { name: string; value: number }[]; regionId: number }) =>
-        city.regionId === region.value,
-    );
-    for (const data of city[0].data) {
+    const city = cities.find(
+      (city: City) => city.regionId === region.value,
+    ) as City;
+
+    for (const data of city.data) {
       await prisma.city.create({
         data: {
           name: data.name,

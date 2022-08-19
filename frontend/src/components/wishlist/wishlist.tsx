03a02ab4524f8be5128fea041f6abeebb6875a04
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-08-19 20:58:36 +0300

    Connect Wishlist component to the store

--- frontend/src/components/wishlist/wishlist.tsx
+++ frontend/src/components/wishlist/wishlist.tsx
@@ -1,31 +1,54 @@
 import {
   ComplectationResponseDto,
   ModelResponseDto,
-} from '@autoline/shared/index';
+  WishlistInput,
+} from '@autoline/shared/common/types/types';
+import { Title } from '@components/common/title/title';
 import { NewCarCard } from '@components/new-car-card/new-car-card';
 import { Grid } from '@mui/material';
-import { useGetWishlistsQuery } from '@store/queries/preferences/wishlist';
+import {
+  useGetWishlistsQuery,
+  useCreateWishlistMutation,
+  useDeleteWishlistMutation,
+} from '@store/queries/preferences/wishlist';
 
 const Wishlist = (): JSX.Element => {
   const { data, isLoading } = useGetWishlistsQuery();
+
+  const [createWishlist] = useCreateWishlistMutation();
+  const [deleteWishlist] = useDeleteWishlistMutation();
+
+  const handleCreate = async (args: WishlistInput): Promise<void> => {
+    await createWishlist(args);
+  };
+
+  const handleDelete = async (wishlistId: string): Promise<void> => {
+    await deleteWishlist(wishlistId);
+  };
 
   return (
     <>
       {!isLoading && (
         <>
-          <h4>LIKED MODELS</h4>
+          <Title element="h4">LIKED MODELS</Title>
           <Grid container spacing={2}>
             {data &&
               data.models &&
               data.models.map((model: ModelResponseDto) => {
                 return (
                   <Grid item xs={12} md={4} key={model.id}>
-                    <NewCarCard {...model} />
+                    <NewCarCard
+                      car={model}
+                      type="model"
+                      deleteWishlist={handleDelete}
+                      createWishlist={handleCreate}
+                      isLiked={true}
+                    />
                   </Grid>
                 );
               })}
           </Grid>
-          <h4>LIKED COMPLECTATIONS</h4>
+          <Title element="h4">LIKED COMPLECTATIONS</Title>
           <Grid container spacing={2}>
             {data &&
               data.complectations &&
@@ -33,7 +56,13 @@ const Wishlist = (): JSX.Element => {
                 (complectation: ComplectationResponseDto) => {
                   return (
                     <Grid item xs={12} md={4} key={complectation.id}>
-                      <NewCarCard {...complectation} />
+                      <NewCarCard
+                        car={complectation}
+                        type="complectation"
+                        deleteWishlist={handleDelete}
+                        createWishlist={handleCreate}
+                        isLiked={true}
+                      />
                     </Grid>
                   );
                 },

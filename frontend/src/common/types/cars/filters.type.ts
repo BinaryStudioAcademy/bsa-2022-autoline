622e21f018fd08100f61cffdc72de126e8ebd99e
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-19 18:33:46 +0300

    Changed common styles, created types,enums

---
+++ frontend/src/common/types/cars/filters.type.ts
@@ -0,0 +1,18 @@
+type FiltersType = {
+  regionId: '';
+  yearStart: '';
+  yearEnd: '';
+  priceStart: '';
+  priceEnd: '';
+  raceStart: '';
+  raceEnd: '';
+  bodytypeId: string[];
+  brandId: string[];
+  modelId: string[];
+  colorId: string[];
+  transmissionTypeId: string[];
+  fueltypeId: string[];
+  drivetrainId: string[];
+};
+
+export type { FiltersType };

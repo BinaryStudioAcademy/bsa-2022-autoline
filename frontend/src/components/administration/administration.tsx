85a9bef46a2370baa6e30b269ec4bc25980fdc1d
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-08-19 10:28:14 +0300

    Fixed undefined `createTheme` exception

--- frontend/src/components/administration/administration.tsx
+++ frontend/src/components/administration/administration.tsx
@@ -1,11 +1,15 @@
+import { ReactElement } from 'react';
+
 import { User } from '@autoline/shared/common/types/types';
-import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
+// If the `Box` is getting imported before the `LinearProgress` the app doesn't work
+// eslint-disable-next-line import/order
+import Box from '@mui/material/Box';
 import { useGetUsersQuery } from '@store/queries/users';
 
 import { UsersList } from './users/users-list';
 
-export const Administration = (): React.ReactElement => {
+export const Administration = (): ReactElement => {
   const { data: users, isLoading, isSuccess } = useGetUsersQuery();
 
   return (

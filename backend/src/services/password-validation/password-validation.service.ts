ab201b8b43f2f58adbc1c68711d5814b924f40cc
Alexander Khapchenko <83529609+AlexanderKhapchenko@users.noreply.github.com>
2022-08-22 19:33:54 +0300

    Fixed one-line funcs
    Co-authored-by: Oleksandr Danylchenko <68850090+oleksandr-danylchenko@users.noreply.github.com>

--- backend/src/services/password-validation/password-validation.service.ts
+++ backend/src/services/password-validation/password-validation.service.ts
@@ -4,8 +4,7 @@ import { compare as bcryptCompare } from 'bcrypt';
 const validatePassword = async (
   password: string,
   userSecurity: User_Security | null | undefined,
-): Promise<boolean> => {
-  return bcryptCompare(password, userSecurity?.password || '');
-};
+): Promise<boolean> =>
+  bcryptCompare(password, userSecurity?.password || '');
 
 export { validatePassword };

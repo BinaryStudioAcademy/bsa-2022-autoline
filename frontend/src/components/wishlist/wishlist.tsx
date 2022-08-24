92fa4bf504f23aef8a9626ead654b5110a7162bc
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-08-24 21:14:18 +0300

    Move Create and Delete functions to Card component

--- frontend/src/components/wishlist/wishlist.tsx
+++ frontend/src/components/wishlist/wishlist.tsx
@@ -1,31 +1,14 @@
 import {
   ComplectationResponseDto,
-  DeleteWishlistInput,
   ModelResponseDto,
-  WishlistInput,
 } from '@autoline/shared/common/types/types';
 import { Title } from '@components/common/title/title';
 import { NewCarCard } from '@components/new-car-card/new-car-card';
 import { Grid } from '@mui/material';
-import {
-  useGetWishlistsQuery,
-  useCreateWishlistMutation,
-  useDeleteWishlistMutation,
-} from '@store/queries/preferences/wishlist';
+import { useGetWishlistsQuery } from '@store/queries/preferences/wishlist';
 
 const Wishlist: React.FC = () => {
   const { data, isLoading } = useGetWishlistsQuery();
-
-  const [createWishlist] = useCreateWishlistMutation();
-  const [deleteWishlist] = useDeleteWishlistMutation();
-
-  const handleCreate = async (args: WishlistInput): Promise<void> => {
-    await createWishlist(args);
-  };
-
-  const handleDelete = async (args: DeleteWishlistInput): Promise<void> => {
-    await deleteWishlist(args);
-  };
 
   if (isLoading) return null;
   return (
@@ -35,13 +18,7 @@ const Wishlist: React.FC = () => {
         {data?.models?.map((model: ModelResponseDto) => {
           return (
             <Grid item xs={12} md={4} key={model.id}>
-              <NewCarCard
-                car={model}
-                type="model"
-                createWishlist={handleCreate}
-                deleteWishlist={handleDelete}
-                isLiked={true}
-              />
+              <NewCarCard car={model} type="model" />
             </Grid>
           );
         })}
@@ -52,13 +29,7 @@ const Wishlist: React.FC = () => {
           (complectation: ComplectationResponseDto) => {
             return (
               <Grid item xs={12} md={4} key={complectation.id}>
-                <NewCarCard
-                  car={complectation}
-                  type="complectation"
-                  createWishlist={handleCreate}
-                  deleteWishlist={handleDelete}
-                  isLiked={true}
-                />
+                <NewCarCard car={complectation} type="complectation" />
               </Grid>
             );
           },

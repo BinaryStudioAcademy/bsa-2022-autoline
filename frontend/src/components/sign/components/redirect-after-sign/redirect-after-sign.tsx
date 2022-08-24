15a082d21fcd2fafb8d3d4da4e3be5e60c4a7496
R0ixy <nikita6kom@gmail.com>
2022-08-24 16:06:40 +0300

    add redirect page

---
+++ frontend/src/components/sign/components/redirect-after-sign/redirect-after-sign.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { StorageKey } from '@common/enums/app/storage-key.enum';
+import { Grid, CircularProgress } from '@mui/material';
+
+const RedirectAfterSign = (): React.ReactElement => {
+  const [searchParams] = useSearchParams();
+  const accessToken = searchParams.get('accessToken');
+  const refreshToken = searchParams.get('refreshToken');
+
+  useEffect(() => {
+    if (accessToken && refreshToken) {
+      localStorage.setItem(StorageKey.TOKEN, accessToken);
+    }
+    window.close();
+  }, []);
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <CircularProgress />
+    </Grid>
+  );
+};
+
+export { RedirectAfterSign };

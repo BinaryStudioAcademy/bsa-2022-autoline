368b75f10c2ff9c4df1f1de4eb7061d4caddc28c
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-24 11:47:17 +0300

    Created car-filter slice & queries for RTK Query

---
+++ frontend/src/store/car-filter/slice.ts
@@ -0,0 +1,44 @@
+import { FiltersNames } from '@common/enums/cars/filters-names.enum';
+import { FiltersType } from '@common/types/cars/filters.type';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState: FiltersType = {
+  regionId: '',
+  yearStart: '',
+  yearEnd: '',
+  priceStart: '',
+  priceEnd: '',
+  raceStart: '',
+  raceEnd: '',
+  bodytypeId: [],
+  brandId: [],
+  modelId: [],
+  colorId: [],
+  transmissionTypeId: [],
+  fueltypeId: [],
+  drivetrainId: [],
+};
+
+const { reducer, actions } = createSlice({
+  name: 'car-filter',
+  initialState,
+  reducers: {
+    setValue: (
+      state,
+      action: PayloadAction<{
+        filterName: FiltersNames;
+        value: string | string[];
+      }>,
+    ) => ({
+      ...state,
+      [action.payload.filterName]: action.payload.value,
+      // ...action.payload,
+    }),
+    resetAllFilters: () => initialState,
+  },
+  extraReducers: {},
+});
+
+export const { setValue, resetAllFilters } = actions;
+
+export { reducer };

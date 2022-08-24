b98cee5ea7cf2dd6e9c26ebe3f1c98fa95a07056
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-24 11:47:17 +0300

    Created car-filter slice & queries for RTK Query

--- frontend/src/hooks/store/store.hooks.ts
+++ frontend/src/hooks/store/store.hooks.ts
@@ -1,7 +1,9 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from '@common/types/types';
+import { AppDispatch } from '@store/store';
 
+const useAppDispatch: () => AppDispatch = useDispatch;
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export { useAppSelector };
+export { useAppDispatch, useAppSelector };

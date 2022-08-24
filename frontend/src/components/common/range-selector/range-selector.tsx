7e83d48e2b69e17c39b39e47747bf6377ddb625c
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-24 11:57:08 +0300

    Created simple-filter & some common components

---
+++ frontend/src/components/common/range-selector/range-selector.tsx
@@ -0,0 +1,83 @@
+import React, { FC, useEffect, useState } from 'react';
+
+import { FiltersNames } from '@common/enums/cars/filters-names.enum';
+import { RangeValueType } from '@common/types/cars/range-item.type';
+import { SelectField } from '@components/common/select-field/select-field';
+import { toCamelCase } from '@helpers/strings';
+import { MenuItem, SelectChangeEvent } from '@mui/material';
+
+type Props = {
+  list: Array<string>;
+  minTitle: string;
+  maxTitle: string;
+  minFilterName: FiltersNames;
+  maxFilterName: FiltersNames;
+  selectedMin: string;
+  selectedMax: string;
+  onChange: (range: RangeValueType[]) => void;
+};
+
+const RangeSelector: FC<Props> = (props) => {
+  const [range, setRange] = useState({
+    min: props.selectedMin,
+    max: props.selectedMax,
+  });
+
+  const handleMinChange = (event: SelectChangeEvent): void => {
+    setRange({
+      ...range,
+      min: event.target.value,
+    });
+  };
+
+  const handleMaxChange = (event: SelectChangeEvent): void => {
+    setRange({
+      ...range,
+      max: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (+range.max && +range.min > +range.max) {
+      [range.min, range.max] = [range.max, range.min];
+    }
+
+    props.onChange([
+      { filterName: props.minFilterName, value: range.min },
+      { filterName: props.maxFilterName, value: range.max },
+    ]);
+  }, [range]);
+
+  return (
+    <>
+      <SelectField
+        id={toCamelCase(props.minTitle)}
+        name={props.minTitle}
+        value={String(props.selectedMin) || ''}
+        onChange={handleMinChange}
+        required={false}
+      >
+        {props.list.map((item, index) => (
+          <MenuItem key={index} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </SelectField>
+      <SelectField
+        id={toCamelCase(props.maxTitle)}
+        name={props.maxTitle}
+        value={String(props.selectedMax) || ''}
+        onChange={handleMaxChange}
+        required={false}
+      >
+        {props.list.map((item, index) => (
+          <MenuItem key={index} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </SelectField>
+    </>
+  );
+};
+
+export { RangeSelector };

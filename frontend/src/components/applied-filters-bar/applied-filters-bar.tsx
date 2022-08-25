c5550796f0a1d63a5cd9e5ffbf5cc02c1e455ee4
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-25 16:33:30 +0300

    Displays applied filters

--- frontend/src/components/applied-filters-bar/applied-filters-bar.tsx
+++ frontend/src/components/applied-filters-bar/applied-filters-bar.tsx
@@ -1,37 +1,93 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
-import { useAppDispatch } from '@hooks/hooks';
+import { filtersToQuery } from '@helpers/filters-to-query';
+import { getValueById } from '@helpers/get-value-by-id';
+import { useAppDispatch, useAppSelector } from '@hooks/hooks';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { Button } from '@mui/material';
-import { PayloadAction } from '@reduxjs/toolkit';
 import { resetAllFilters } from '@store/car-filter/slice';
+import {
+  useGetBrandsQuery,
+  useGetModelsOfBrandQuery,
+  useGetUsedOptionsQuery,
+} from '@store/queries/cars';
 
 import styles from './styles.module.scss';
 
 const AppliedFiltersBar: FC = () => {
   const dispatch = useAppDispatch();
 
-  const mocked = ['Sedan', 'Lviv', '2014-2018'];
+  const [appliedFIlters, setAppliedFilters] = useState<string[][]>([]);
 
-  const resetFilters = (): PayloadAction => dispatch(resetAllFilters());
+  const filters = useAppSelector((state) => state.carFilter);
+
+  const { data: brands } = useGetBrandsQuery();
+
+  const { data: models } = useGetModelsOfBrandQuery(filters.brandId[0], {
+    skip: !filters.brandId.length,
+  });
+
+  const { data: options } = useGetUsedOptionsQuery();
+
+  const deleteFilter = (filterName: string, id: string): void => {
+    // eslint-disable-next-line no-console
+    console.log(filterName);
+    // eslint-disable-next-line no-console
+    console.log(id);
+  };
+
+  useEffect(() => {
+    if (brands && options) {
+      const allOptions = Object.values(options)
+        .flatMap((item) => item)
+        .concat(brands)
+        .concat(models || []);
+
+      const filtersArr = filtersToQuery(filters);
+
+      //MAP!!!!!!!!!!!!!!
+      // const withLabels = filtersArr.map(([filterName, id, label]) => {
+      const withLabels = filtersArr.map(([filterName, id]) => {
+        if (filterName.includes('year')) {
+          const { yearStart, yearEnd } = filters;
+          return [filterName, id, `${yearStart} - ${yearEnd}`];
+        }
+
+        const value = getValueById(allOptions, id);
+
+        return [filterName, id, value?.label || ''];
+      });
+
+      setAppliedFilters(withLabels);
+    }
+  }, [filters]);
+
+  const resetFilters = (): void => {
+    dispatch(resetAllFilters());
+  };
 
   return (
     <div className={styles.container}>
-      {mocked.map((item, index) => (
-        <div className={styles.item} key={index}>
-          {item}
-          <HighlightOffIcon />
+      {appliedFIlters.map(([filterName, id, label]) => (
+        <div className={styles.item} key={id}>
+          {label}
+          <HighlightOffIcon
+            onClick={(): void => deleteFilter(filterName, id)}
+          />
         </div>
       ))}
-      <Button
-        onClick={resetFilters}
-        className={styles.deleteButton}
-        variant="contained"
-        startIcon={<DeleteOutlinedIcon className={styles.deleteIcon} />}
-      >
-        Delete all
-      </Button>
+
+      {!!appliedFIlters.length && (
+        <Button
+          onClick={resetFilters}
+          className={styles.deleteButton}
+          variant="contained"
+          startIcon={<DeleteOutlinedIcon className={styles.deleteIcon} />}
+        >
+          Delete all
+        </Button>
+      )}
     </div>
   );
 };

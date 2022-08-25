de50528a80c9f46f1de5a9084128e78271e95fa5
R0ixy <nikita6kom@gmail.com>
2022-08-25 20:27:34 +0300

    update CarsSearchParams

--- shared/src/common/types/cars/cars.ts
+++ shared/src/common/types/cars/cars.ts
@@ -1,3 +1,4 @@
 export type { BrandType } from './brand';
 export type { ModelType } from './model';
 export type { OptionsType, AutoRiaOption } from './options';
+export type { CarsSearchParams } from './cars-search-params';

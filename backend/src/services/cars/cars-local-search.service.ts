f07b3dd13313e09979b1e08f55fc914e203f8da5
R0ixy <nikita6kom@gmail.com>
2022-08-26 12:09:12 +0300

    update search query

--- backend/src/services/cars/cars-local-search.service.ts
+++ backend/src/services/cars/cars-local-search.service.ts
@@ -2,12 +2,21 @@ import { CarsSearchParams } from '@autoline/shared';
 import { SearchResult } from '@common/types/types';
 import { prisma } from '@data/prisma-client';
 
-const carsSearch = async (data: CarsSearchParams): Promise<SearchResult[]> =>
-  await prisma.model.findMany({
+const carsSearch = async (data: CarsSearchParams): Promise<SearchResult[]> => {
+  return prisma.model.findMany({
     include: {
       body_type: true,
       brand: true,
-      prices_ranges: true,
+      prices_ranges: {
+        where: {
+          price_start: {
+            gte: data.priceStart ? +data.priceStart : undefined,
+          },
+          price_end: {
+            lte: data.priceEnd ? +data.priceEnd : undefined,
+          },
+        },
+      },
       complectations: {
         where: {
           color_id: {
@@ -49,13 +58,7 @@ const carsSearch = async (data: CarsSearchParams): Promise<SearchResult[]> =>
       year_start: {
         gte: data.yearStart ? +data.yearStart : undefined,
       },
-      year_end: {
-        lte: data.yearEnd
-          ? +data.yearEnd === new Date().getFullYear()
-            ? undefined
-            : +data.yearEnd
-          : undefined,
-      },
+      year_end: getYearEndCondition(data.yearEnd),
       prices_ranges: {
         some: {
           price_start: {
@@ -84,5 +87,22 @@ const carsSearch = async (data: CarsSearchParams): Promise<SearchResult[]> =>
       },
     },
   });
+};
+
+const getYearEndCondition = (
+  yearEnd: string,
+): null | { lte: number | undefined } => {
+  let yearEndCondition;
+
+  if (+yearEnd === new Date().getFullYear()) {
+    yearEndCondition = null;
+  } else {
+    yearEndCondition = {
+      lte: yearEnd ? +yearEnd : undefined,
+    };
+  }
+
+  return yearEndCondition;
+};
 
 export { carsSearch };

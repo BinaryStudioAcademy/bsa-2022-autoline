8dc532a5be9c8ce42f7c29f0c8427acf7c3f57a9
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-27 19:34:02 +0300

    Changed filter slice structure, added actions

--- frontend/src/store/car-filter/slice.ts
+++ frontend/src/store/car-filter/slice.ts
@@ -1,17 +1,26 @@
-import { FiltersNames } from '@common/enums/cars/filters-names.enum';
-// import { FiltersType } from '@common/types/cars/filters.type';
-import { BrandDetailsType } from '@common/types/cars/brand-details.type';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  CheckListsNames,
+  RangeNames,
+} from '@common/enums/cars/filter-names.enum';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  filters: {
-    regionId: '',
-    yearStart: '',
-    yearEnd: '',
-    priceStart: '',
-    priceEnd: '',
-    raceStart: '',
-    raceEnd: '',
+  rangeFilters: {
+    year: {
+      yearStart: '',
+      yearEnd: '',
+    },
+    price: {
+      priceStart: '',
+      priceEnd: '',
+    },
+    race: {
+      raceStart: '',
+      raceEnd: '',
+    },
+  },
+  checkLists: {
+    regionId: [],
     bodytypeId: [],
     colorId: [],
     transmissionTypeId: [],
@@ -23,6 +32,8 @@ const initialState = {
       id: Date.now().toString(),
       brandId: '',
       modelId: '',
+      brandName: '',
+      modelName: '',
     },
   ],
 };
@@ -31,31 +42,60 @@ const { reducer, actions } = createSlice({
   name: 'car-filter',
   initialState,
   reducers: {
-    setValue: (
-      state,
-      action: PayloadAction<{
-        filterName: FiltersNames;
-        value: string | string[];
-      }>,
-    ) => {
-      state.filters = {
-        ...state.filters,
+    setRangeValue: (state, action) => {
+      const { values } = action.payload;
+      const rangeName = action.payload.rangeName as RangeNames;
+      state.rangeFilters[rangeName] = values;
+    },
+    setCheckListValue: (state, action) => {
+      state.checkLists = {
+        ...state.checkLists,
         [action.payload.filterName]: action.payload.value,
       };
     },
+    setBrandDetailsValue: (state, action) => {
+      const needle = state.brandDetails.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+
+      if (needle !== -1) {
+        state.brandDetails[needle] = action.payload;
+      }
+    },
     addNewBrandDetails: ({ brandDetails }) => {
       brandDetails.push({
+        ...initialState.brandDetails[0],
         id: Date.now().toString(),
-        brandId: '',
-        modelId: '',
       });
     },
-    changeBrandDetails: (state, action: PayloadAction<BrandDetailsType>) => {
-      const old = state.brandDetails
-        .filter((item) => item.id !== action.payload.id)
-        .sort((a, b) => parseInt(a.id) - parseInt(b.id));
+    removeRangeFilter: (state, action) => {
+      const rangeName = action.payload.filterName as RangeNames;
+      state.rangeFilters = {
+        ...state.rangeFilters,
+        [rangeName]: initialState.rangeFilters[rangeName],
+      };
+    },
+    removeCheckListFilter: (state, action) => {
+      const { id } = action.payload;
+      const filterName = action.payload.filterName as CheckListsNames;
 
-      state.brandDetails = [...old, action.payload];
+      state.checkLists[filterName] = state.checkLists[filterName].filter(
+        (item) => item !== id,
+      );
+    },
+    removeBrandDetailsFilter: (state, action) => {
+      const { filterName, id } = action.payload;
+
+      if (filterName === 'brandId') {
+        state.brandDetails = state.brandDetails.filter(
+          (details) => details.brandId !== id,
+        );
+      }
+      if (filterName === 'modelId') {
+        state.brandDetails.forEach(
+          (detail) => detail.modelId === id && (detail.modelId = ''),
+        );
+      }
     },
     resetAllFilters: () => initialState,
   },
@@ -63,10 +103,14 @@ const { reducer, actions } = createSlice({
 });
 
 export const {
-  setValue,
+  setCheckListValue,
+  setRangeValue,
   addNewBrandDetails,
-  changeBrandDetails,
+  setBrandDetailsValue,
   resetAllFilters,
+  removeCheckListFilter,
+  removeBrandDetailsFilter,
+  removeRangeFilter,
 } = actions;
 
 export { reducer };

bc3f0a53f187c5eeb5bd3caa929bbc52805b49f5
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-08-28 13:27:24 +0300

    Display props in Complete Set Table

---
+++ frontend/src/components/complete-set-table/complete-set-table-collapsed.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+import { ComplectationDetailsType } from '@autoline/shared/common/types/types';
+import { CompleteSetPropsType } from '@common/types/types';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import { Collapse } from '@mui/material';
+
+import { CompleteSetTable } from './complete-set-table';
+import styles from './styles.module.scss';
+
+const CompleteSetTableCollapsed: React.FC<CompleteSetPropsType> = (props) => {
+  const { className, data } = props;
+
+  const initialRows = 5;
+  const [carsDisplayed, setCarsDisplayed] = useState<
+    ComplectationDetailsType[]
+  >([]);
+  const [rowsHidden, setRowsHidden] = useState(0);
+
+  useEffect(() => {
+    setCarsDisplayed(data?.slice(0, initialRows));
+    if (data && data.length > initialRows) {
+      setRowsHidden(data.length - initialRows);
+    }
+  }, [data]);
+
+  const [open, setOpen] = useState(false);
+  const handleClick = (): void => {
+    open
+      ? setCarsDisplayed(data?.slice(0, initialRows))
+      : setCarsDisplayed(data);
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      {rowsHidden > 0 ? (
+        <>
+          <Collapse in={open} timeout="auto" collapsedSize="290px">
+            <CompleteSetTable data={carsDisplayed} className={className} />
+          </Collapse>
+          <button className={styles.collapseButton} onClick={handleClick}>
+            + {rowsHidden} {open ? <ExpandLess /> : <ExpandMore />}
+          </button>
+        </>
+      ) : (
+        <CompleteSetTable data={carsDisplayed} className={className} />
+      )}
+    </>
+  );
+};
+
+export { CompleteSetTableCollapsed };

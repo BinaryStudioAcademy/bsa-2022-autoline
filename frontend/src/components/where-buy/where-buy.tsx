90b93acee1fd44d8886902857ed25e507c16c297
setar92 <setarasiuk@gmail.com>
2022-08-28 19:11:22 +0300

    where to buy element close to be done

---
+++ frontend/src/components/where-buy/where-buy.tsx
@@ -0,0 +1,35 @@
+import { ButtonOutline } from '@components/common/button-outline/button-outline';
+import Divider from '@mui/material/Divider';
+
+import styles from './styles.module.scss';
+import { WhereToBuyComponent } from './where-buy-component/where-buy-component';
+
+const WhereToBuy: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <div className={styles.carName}>bmw x5 m packet 40d 2015</div>
+        <ButtonOutline className={styles.priceButton} text="By Price" />
+      </div>
+      <WhereToBuyComponent />
+      <div className={styles.dividerDiv}>
+        <Divider className={styles.divider} />
+      </div>
+      <WhereToBuyComponent />
+      <div className={styles.dividerDiv}>
+        <Divider className={styles.divider} />
+      </div>
+      <WhereToBuyComponent />
+      <div className={styles.dividerDiv}>
+        <Divider className={styles.divider} />
+      </div>
+      <WhereToBuyComponent />
+      <div className={styles.dividerDiv}>
+        <Divider className={styles.divider} />
+      </div>
+      <WhereToBuyComponent />
+    </div>
+  );
+};
+
+export { WhereToBuy };

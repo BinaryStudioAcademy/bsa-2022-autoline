1099df894292e363c326e0095358e4831163eb11
Stanislav-Ostr <stanislav.ostr@gmail.com>
2022-08-29 11:35:54 +0300

    create-layout-for-landing-page

--- frontend/src/components/landing-page/components/drawer/drawer.tsx
+++ frontend/src/components/landing-page/components/drawer/drawer.tsx
@@ -10,7 +10,7 @@ import {
   ListItemText,
 } from '@mui/material';
 
-const pages = ['Used Cars', 'New Cars', 'Sell Your Car', 'About Us'];
+const pages = ['Used Cars', 'New Cars', 'About Us'];
 
 export const DrawerComp = (): React.ReactElement => {
   const [openDrawer, setOpenDrawer] = useState(false);

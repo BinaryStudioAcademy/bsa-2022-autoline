233dd0bc71e1f52978b6e0c83acf80ad3174d0a2
R0ixy <nikita6kom@gmail.com>
2022-08-30 18:40:21 +0300

    update jwt exception

--- backend/src/helpers/token/verify-token/verify-token.helper.ts
+++ backend/src/helpers/token/verify-token/verify-token.helper.ts
@@ -1,13 +1,14 @@
 import { ENV } from '@common/enums/app/env.enum';
 import { HttpError } from '@dtos/execptions/error.dto';
+import httpStatus from 'http-status-codes';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const verifyToken = (token: string): string | JwtPayload => {
   try {
     return jwt.verify(token, ENV.JWT.SECRET as string);
   } catch (error) {
-    if (error instanceof Error && error.name === 'TokenExpiredError') {
-      throw new HttpError(error, 401);
+    if (error instanceof Error) {
+      throw new HttpError(error, httpStatus.UNAUTHORIZED);
     } else {
       throw error;
     }

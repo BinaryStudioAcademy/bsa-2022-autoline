d7d8b87c8e6012232002b250c6bb3c3c37ce92cc
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-08-30 14:00:19 +0300

    fix/changed min requered characters and matches the fullname input field

--- shared/src/validation-schemas/sign-up/sign-up.validation-schema.ts
+++ shared/src/validation-schemas/sign-up/sign-up.validation-schema.ts
@@ -4,12 +4,9 @@ import * as Yup from 'yup';
 const signUpSchema = Yup.object().shape({
   name: Yup.string()
     .required('Full name is required')
-    .min(2, 'Full name must be at least 2 characters')
+    .min(1, 'Full name must be at least 1 characters')
     .max(150, 'Full name must not exceed 150 characters')
-    .matches(
-      /^[A-Za-z]+[A-Za-z\s]+[A-Za-z]+$/,
-      'Full name must contain only latin characters',
-    ),
+    .matches(/^[A-Za-z\s]+$/, 'Full name must contain only latin characters'),
   email: Yup.string()
     .required(UserValidationMessage.EMAIL_REQUIRED)
     .email(UserValidationMessage.INVALID_EMAIL)

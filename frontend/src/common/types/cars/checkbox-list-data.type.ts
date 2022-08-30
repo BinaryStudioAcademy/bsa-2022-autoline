43435e322bcccb2e3c1db7d465760e143103a4f8
Andrii Ilnitskyi <elador97@gmail.com>
2022-08-30 17:56:29 +0300

    path style fix

--- frontend/src/common/types/cars/checkbox-list-data.type.ts
+++ frontend/src/common/types/cars/checkbox-list-data.type.ts
@@ -1,4 +1,4 @@
-import { CheckListsNames } from '../../enums/car/car-filters-names.enum';
+import { CheckListsNames } from '@common/enums/car/car-filters-names.enum';
 
 export type CheckboxListDataType = {
   filterName: CheckListsNames;

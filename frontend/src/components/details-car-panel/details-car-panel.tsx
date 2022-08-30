4da5fff7f3c2929866ccddbc8fb44697f2fe1eb5
brovarenko <brovwg@gmail.com>
2022-08-30 19:22:48 +0300

    fixed file name

--- frontend/src/components/details-car-panel/details-car-panel.tsx
+++ frontend/src/components/details-car-panel/details-car-panel.tsx
@@ -6,7 +6,7 @@ import {
 } from '@autoline/shared/common/types/types';
 import { DetailsCarPanelPropsType } from '@common/types/types';
 import { HeartIcon } from '@components/common/icons/icons';
-import { convertPrice } from '@helpers/utils/convertPrice';
+import { convertPrice } from '@helpers/utils/convert-price';
 import {
   useGetComplectationsForPanelQuery,
   useGetRateQuery,

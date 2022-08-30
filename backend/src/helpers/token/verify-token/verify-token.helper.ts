3616ebfc42fa4e33eb36d8317cae76e33f368d05
R0ixy <nikita6kom@gmail.com>
2022-08-30 10:47:16 +0300

    update jwt error handling

--- backend/src/helpers/token/verify-token/verify-token.helper.ts
+++ backend/src/helpers/token/verify-token/verify-token.helper.ts
@@ -1,7 +1,17 @@
 import { ENV } from '@common/enums/app/env.enum';
+import { HttpError } from '@dtos/execptions/error.dto';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
-const verifyToken = (token: string): string | JwtPayload =>
-  jwt.verify(token, ENV.JWT.SECRET as string);
+const verifyToken = (token: string): string | JwtPayload => {
+  try {
+    return jwt.verify(token, ENV.JWT.SECRET as string);
+  } catch (error) {
+    if (error instanceof Error && error.name === 'TokenExpiredError') {
+      throw new HttpError(error, 401);
+    } else {
+      throw error;
+    }
+  }
+};
 
 export { verifyToken };

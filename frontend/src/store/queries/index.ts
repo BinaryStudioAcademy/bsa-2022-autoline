263eed0094608bdf61217be39463a71cc0badfc0
R0ixy <nikita6kom@gmail.com>
2022-08-30 10:35:13 +0300

    add baseQuery with reauth

--- frontend/src/store/queries/index.ts
+++ frontend/src/store/queries/index.ts
@@ -5,7 +5,9 @@ import {
   fetchBaseQuery,
   BaseQueryFn,
   FetchArgs,
+  FetchBaseQueryError,
 } from '@reduxjs/toolkit/query/react';
+import { setCredentials, logOut } from '@store/root-reducer';
 
 export type ErrorType = {
   data: {
@@ -25,8 +27,35 @@ const baseQuery = fetchBaseQuery({
   },
 }) as BaseQueryFn<string | FetchArgs, unknown, ErrorType>;
 
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    const refreshResult = await baseQuery(
+      {
+        url: '/auth/refreshToken',
+        method: 'POST',
+        body: { refreshToken: localStorage.getItem('refresh') },
+      },
+      api,
+      extraOptions,
+    );
+    if (refreshResult.data) {
+      api.dispatch(setCredentials(refreshResult.data));
+      localStorage.setItem('token', refreshResult.data as string);
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
+
 export const api = createApi({
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['WishlistCars', 'User', 'Cars'],
   endpoints: () => ({}),
 });

aa2aef2fec244741f7a4084f2f1914c013a94666
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-08-31 12:40:19 +0300

    Revert "Fix retrieving of user role"
    This reverts commit c17da985762119566f99d9b6378762330230e8d8.

--- frontend/src/navigation/routing/routing.tsx
+++ frontend/src/navigation/routing/routing.tsx
@@ -14,15 +14,13 @@ import { RedirectAfterSign } from '@components/sign/components/redirect-after-si
 import { Sign } from '@components/sign/sign';
 import { useAppSelector } from '@hooks/hooks';
 import { ProtectedRoute } from '@navigation/protected-route/protected-route';
-import jwt_decode from 'jwt-decode';
+import { useGetUserQuery } from '@store/queries/user/update-user';
 
 const Routing: FC = () => {
+  const { data: authData } = useGetUserQuery();
+  const isAdmin = authData?.role === 'admin';
+
   const userToken = useAppSelector((state) => state.auth.token);
-  let isAdmin;
-  if (userToken) {
-    const user = jwt_decode(userToken) as { role: string };
-    isAdmin = user?.role === 'admin';
-  }
 
   return (
     <BrowserRouter>
@@ -59,7 +57,7 @@ const Routing: FC = () => {
         <Route element={<ProtectedRoute isAllowed={!!userToken} />}>
           <Route path={AppRoute.ROOT} element={<LandingPage />} />
         </Route>
-        <Route element={<ProtectedRoute isAllowed={!!isAdmin} />}>
+        <Route element={<ProtectedRoute isAllowed={isAdmin} />}>
           <Route path={AppRoute.ADMINISTRATION} element={<Administration />} />
         </Route>
         <Route element={<ProtectedRoute isAllowed={!!userToken} />}>

c1e2b8c9c6c94fddb5b18685a255989e6e59d81f
Oleksii Rudych <94957824+o-rudych@users.noreply.github.com>
2022-09-01 17:36:00 +0300

    added better view for No cars found and top autoria cars added

--- frontend/src/components/search-page/search-page.tsx
+++ frontend/src/components/search-page/search-page.tsx
@@ -6,7 +6,13 @@ import { CarListItem } from '@components/car-list-item/car-list-item';
 import { PageContainer } from '@components/common/page-container/page-container';
 import { Title } from '@components/common/title/title';
 import { Header } from '@components/header/header';
-import { Grid } from '@mui/material';
+import { topCars } from '@components/landing-page/mock-top-cars';
+import { TopCarCard } from '@components/top-car-card/top-car-card';
+import SearchIcon from '@mui/icons-material/Search';
+import { Container, Grid } from '@mui/material';
+import clsx from 'clsx';
+
+import styles from './styles.module.scss';
 
 const SearchPage: React.FC = () => {
   const [cars, setCars] = useState<FilterReturnType>([]);
@@ -21,7 +27,26 @@ const SearchPage: React.FC = () => {
         <Title element="h3">Search</Title>
         <Grid container spacing={2}>
           <Grid item xs={12} md={8}>
-            {cars.length === 0 && <p>No cars found.</p>}
+            {cars.length === 0 && (
+              <>
+                <Container sx={{ display: 'flex' }}>
+                  <SearchIcon
+                    className={clsx(styles.searchIcon, styles.icon)}
+                  />
+                  <h3 className={styles.noCarsTitle}>No cars found</h3>
+                </Container>
+                <Container className={styles.topAutoriaContainer}>
+                  <div>
+                    <div className={styles.topAutoriaHeader}>Top Autoria</div>
+                    <div className={styles.topAutoriaCards}>
+                      {topCars.map((car) => (
+                        <TopCarCard car={car} key={car.id} />
+                      ))}
+                    </div>
+                  </div>
+                </Container>
+              </>
+            )}
             {cars.map((car) => (
               <CarListItem {...car} key={car.model_id} />
             ))}

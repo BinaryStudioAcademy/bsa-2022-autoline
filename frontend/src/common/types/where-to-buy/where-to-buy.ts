9b757e76f489cb55f6078869978c419b3b048304
setar92 <setarasiuk@gmail.com>
2022-09-02 17:27:19 +0300

    added mock data

--- frontend/src/common/types/where-to-buy/where-to-buy.ts
+++ frontend/src/common/types/where-to-buy/where-to-buy.ts
@@ -2,6 +2,8 @@ interface WhereBuyItemProps {
   carName: string;
   url: string;
   price: number;
+  workYears: number;
+  id: string;
 }
 
 export { type WhereBuyItemProps };
--- frontend/src/components/where-to-buy/mock-value.ts
+++ frontend/src/components/where-to-buy/mock-value.ts
@@ -4,27 +4,37 @@ const mockValue: WhereBuyItemProps[] = [
   {
     carName: 'bmw x5 m packet 40d 2015',
     url: 'auto.ria.com',
-    price: 47500,
+    price: 46500,
+    id: 'asdsfs45s6df',
+    workYears: 5,
   },
   {
     carName: 'bmw x5 m packet 40d 2015',
     url: 'auto.ria.com',
-    price: 47500,
+    price: 48500,
+    id: 'askjd56s4f6',
+    workYears: 3,
   },
   {
     carName: 'bmw x5 m packet 40d 2015',
     url: 'auto.ria.com',
-    price: 47500,
+    price: 47200,
+    id: 'sdfsdf4561',
+    workYears: 4,
   },
   {
     carName: 'bmw x5 m packet 40d 2015',
     url: 'auto.ria.com',
-    price: 47500,
+    price: 47900,
+    id: 'sdfgdf7845',
+    workYears: 1,
   },
   {
     carName: 'bmw x5 m packet 40d 2015',
     url: 'auto.ria.com',
-    price: 47500,
+    price: 48000,
+    id: 'asddfgfd784',
+    workYears: 3,
   },
 ];
 

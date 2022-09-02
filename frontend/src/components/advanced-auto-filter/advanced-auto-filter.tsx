764ece87347ccbc1c1c6295b65ba9172444c8831
Andrii Ilnitskyi <elador97@gmail.com>
2022-09-02 10:25:45 +0300

    Smooth scrolling to top after search click

--- frontend/src/components/advanced-auto-filter/advanced-auto-filter.tsx
+++ frontend/src/components/advanced-auto-filter/advanced-auto-filter.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect, useMemo, useState } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 import { AutoRiaOption } from '@autoline/shared/common/types/types';
 import {
@@ -57,6 +58,7 @@ const AdvancedAutoFilter: FC<AdvancedAutoFilterProps> = (props) => {
 
   const doSearch = (): void => {
     search(queryParams);
+    scroll.scrollToTop({ duration: 700 });
   };
 
   useEffect(() => {

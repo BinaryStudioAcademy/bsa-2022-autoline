f097144cd4135d1c98fb293113a2ece6ea5b1a79
Andrii Ilnitskyi <elador97@gmail.com>
2022-09-02 11:47:31 +0300

    Smooth scrolling on window.scrollTo

--- frontend/src/components/advanced-auto-filter/advanced-auto-filter.tsx
+++ frontend/src/components/advanced-auto-filter/advanced-auto-filter.tsx
@@ -57,6 +57,10 @@ const AdvancedAutoFilter: FC<AdvancedAutoFilterProps> = (props) => {
 
   const doSearch = (): void => {
     search(queryParams);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   };
 
   useEffect(() => {

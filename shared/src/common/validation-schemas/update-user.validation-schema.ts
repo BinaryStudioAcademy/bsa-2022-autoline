c388c6be92e6cfa5f5818b7a2bfb81ec3e93f9de
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2022-09-04 15:20:02 +0300

    Fixed full name validation

--- shared/src/common/validation-schemas/update-user.validation-schema.ts
+++ shared/src/common/validation-schemas/update-user.validation-schema.ts
@@ -8,7 +8,13 @@ const phoneReg = /^(?:\+38)?(0[5-9][0-9]\d{7})$/gm;
 const updateUserSchema = Yup.object().shape({
   name: Yup.string()
     .required('Full name is required')
-    .max(150, 'Full name must not exceed 100 characters'),
+    .min(2, 'Full name must be at least 2 characters long')
+    .max(150, 'Full name must not exceed 150 characters')
+    .matches(
+      /^\s*([a-zA-Z]+\s)*[a-zA-Z]*\s*$/,
+      'Full name must contain only latin characters',
+    )
+    .trim(),
   email: Yup.string()
     .required(UserValidationMessage.EMAIL_REQUIRED)
     .email(UserValidationMessage.INVALID_EMAIL)

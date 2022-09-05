c67f8c6344506f1ea4071114a476bcbf66553752
Oleksii Rudych <94957824+o-rudych@users.noreply.github.com>
2022-09-06 00:10:27 +0300

    moved verification of an existing user with the same e-mail to the service

--- backend/src/middlewares/users/edit-user.middleware.ts
+++ backend/src/middlewares/users/edit-user.middleware.ts
@@ -1,7 +1,6 @@
 import { ErrorMessage, editUserSchema } from '@autoline/shared';
 import { TypedRequest } from '@common/types/controller/controller';
 import { UserUpdateInput } from '@controllers/users/users.controller';
-import { prisma } from '@data/prisma-client';
 import { NextFunction, Response } from 'express';
 import httpStatus from 'http-status-codes';
 
@@ -12,16 +11,6 @@ const editUserMiddleware = async (
 ): Promise<void> => {
   try {
     await editUserSchema.validate(req.body);
-    const existedUser = await prisma.user.findFirst({
-      where: {
-        id: {
-          not: req.params.id,
-        },
-        email: req.body.email,
-      },
-    });
-    if (existedUser) next(new Error('User with this email already exists!'));
-
     next();
   } catch (err) {
     const { message } = err as ErrorMessage;

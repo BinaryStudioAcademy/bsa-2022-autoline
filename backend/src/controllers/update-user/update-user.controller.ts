5c8aa5e4abce1102b82d5ee212ef48ebe932becf
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2022-09-07 21:49:30 +0300

    Fixed requested changes

--- backend/src/controllers/update-user/update-user.controller.ts
+++ backend/src/controllers/update-user/update-user.controller.ts
@@ -1,20 +1,16 @@
-import fs from 'fs';
-import stream from 'stream';
-
 import { TokenPayload } from '@autoline/shared';
 import { S3Folders } from '@common/enums/aws/aws';
 import { ExceptionMessage } from '@common/enums/exception/exception-message.enum';
 import { ProfileImageSize } from '@common/enums/image/image';
 import { TypedRequestBody } from '@common/types/controller/controller';
 import { UpdateUserDto } from '@dtos/user/update-user.dto';
-import { UsersHelper } from '@helpers/helpers';
+import { usersHelper } from '@helpers/helpers';
 import { Role, Sex } from '@prisma/client';
-import { uploadFileToS3 } from '@services/aws/aws.service';
+import { generateS3Key, uploadFileToS3 } from '@services/aws/aws.service';
+import { resizePhoto } from '@services/photo/photo.service';
 import * as userService from '@services/user/user.service';
 import { NextFunction, Response } from 'express';
 import httpStatus from 'http-status-codes';
-import sharp from 'sharp';
-import { v4 as uuid } from 'uuid';
 
 export interface UpdateUserReq {
   tokenPayload: TokenPayload;
@@ -105,18 +101,13 @@ const updateUserPhoto = async (
     const file = req.files.photo;
     if (!file) throw new Error('No file provided');
 
-    const readableStream = fs.createReadStream(file.path);
-    const fileName = `${uuid()}.${file.type.split('/')[1]}`;
-    const s3Key = `${S3Folders.USER_IMAGES}/${fileName}`;
-    const transformer = sharp().resize({
+    const s3Key = generateS3Key(S3Folders.USER_IMAGES, file.type);
+    const writableStream = resizePhoto(file, {
       width: ProfileImageSize.WIDTH,
       height: ProfileImageSize.HEIGHT,
     });
-    const writableStream = new stream.PassThrough();
-    readableStream.pipe(transformer).pipe(writableStream);
-
     const photoUrl = await uploadFileToS3(writableStream, s3Key);
-    await UsersHelper.deleteUserPhoto(req.body.tokenPayload.sub);
+    await usersHelper.deleteUserPhoto(req.body.tokenPayload.sub);
     await userService.updateUserPhoto(req.body.tokenPayload.sub, photoUrl);
 
     return res.status(httpStatus.OK).json({ photoUrl });
@@ -131,7 +122,7 @@ const deleteUserPhoto = async (
   next: NextFunction,
 ): Promise<Response | undefined> => {
   try {
-    await UsersHelper.deleteUserPhoto(req.body.tokenPayload.sub);
+    await usersHelper.deleteUserPhoto(req.body.tokenPayload.sub);
     return res.status(httpStatus.OK).json();
   } catch (error) {
     next(error);

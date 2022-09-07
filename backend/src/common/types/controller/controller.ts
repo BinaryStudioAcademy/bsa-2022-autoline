f2274327dccc421bd93455fb58710ec0c46af68f
R0ixy <nikita6kom@gmail.com>
2022-09-07 11:01:09 +0300

    globally add tokenPayload to plain request

--- backend/src/common/types/controller/controller.ts
+++ backend/src/common/types/controller/controller.ts
@@ -2,6 +2,15 @@ import { TokenPayload } from '@autoline/shared';
 import { Request } from 'express';
 import { Query, ParamsDictionary } from 'express-serve-static-core';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      tokenPayload: TokenPayload;
+    }
+  }
+}
+
 interface TypedRequestBody<T> extends Request {
   body: T;
 }
@@ -19,28 +28,9 @@ interface TypedRequest<T extends Query, U> extends Request {
   query: T;
 }
 
-interface AuthRequest extends Request {
-  tokenPayload: TokenPayload;
-}
-
-type AuthTypedRequestBody<T> = TypedRequestBody<T> & AuthRequest;
-
-type AuthTypedRequestQuery<T extends Query> = TypedRequestQuery<T> &
-  AuthRequest;
-
-type AuthTypedRequestParams<T extends ParamsDictionary> =
-  TypedRequestParams<T> & AuthRequest;
-
-type AuthTypedRequest<T extends Query, U> = TypedRequest<T, U> & AuthRequest;
-
 export type {
   TypedRequestBody,
   TypedRequestQuery,
   TypedRequestParams,
   TypedRequest,
-  AuthRequest,
-  AuthTypedRequestBody,
-  AuthTypedRequestQuery,
-  AuthTypedRequestParams,
-  AuthTypedRequest,
 };

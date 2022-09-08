20b2356935542fe5e5e62ca17c4ccb84a51ee7c1
Andrii Ilnitskyi <elador97@gmail.com>
2022-09-08 17:46:29 +0300

    Fixed brands slider (slidesPerView)

--- frontend/src/components/cars-categories/swiper-params.ts
+++ frontend/src/components/cars-categories/swiper-params.ts
@@ -4,4 +4,21 @@ export const swiperParams: SwiperOptions = {
   loop: true,
   mousewheel: true,
   modules: [Navigation, Mousewheel],
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+    },
+    640: {
+      slidesPerView: 4,
+    },
+    820: {
+      slidesPerView: 5,
+    },
+    1080: {
+      slidesPerView: 6,
+    },
+    1300: {
+      slidesPerView: 8,
+    },
+  },
 };

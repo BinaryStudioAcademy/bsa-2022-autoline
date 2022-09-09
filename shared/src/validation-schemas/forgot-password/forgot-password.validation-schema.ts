ab66f04c95a92ed4920ccd9bd7f921553c8491c1
ashonuyak <ashonuyak@gmail.com>
2022-09-09 16:27:05 +0300

    fix max password length

--- shared/src/validation-schemas/forgot-password/forgot-password.validation-schema.ts
+++ shared/src/validation-schemas/forgot-password/forgot-password.validation-schema.ts
@@ -1,6 +1,9 @@
 import * as Yup from 'yup';
 
-import { UserValidationMessage } from '../../common/enums/enums';
+import {
+  UserValidationMessage,
+  UserValidationRule,
+} from '../../common/enums/enums';
 
 const forgotPassword = Yup.object().shape({
   email: Yup.string()
@@ -10,8 +13,14 @@ const forgotPassword = Yup.object().shape({
 
 const resetPassword = Yup.object().shape({
   password: Yup.string()
-    .min(8, UserValidationMessage.PASSWORD_MIN_LENGTH)
-    .max(150, UserValidationMessage.PASSWORD_MAX_LENGTH)
+    .min(
+      UserValidationRule.PASSWORD_MIN_LENGTH,
+      UserValidationMessage.PASSWORD_MIN_LENGTH,
+    )
+    .max(
+      UserValidationRule.PASSWORD_MAX_LENGTH,
+      UserValidationMessage.PASSWORD_MAX_LENGTH,
+    )
     .required(UserValidationMessage.PASSWORD_REQUIRED),
   confirmPwd: Yup.string()
     .oneOf([Yup.ref('password')], UserValidationMessage.PASSWORDS_NOT_MATCH)

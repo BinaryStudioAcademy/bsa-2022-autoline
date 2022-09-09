34ecf06e3a67b1f7ded6249ae9d69af6bb5aa1a6
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-09-09 15:43:03 +0300

    changed carCard component for receiving data via props

--- frontend/src/components/comparison-popup/car-card/car-card.tsx
+++ frontend/src/components/comparison-popup/car-card/car-card.tsx
@@ -1,19 +1,49 @@
-import { FC } from 'react';
+import { FC, Dispatch } from 'react';
 
-import carImg from '@assets/images/mock_car_picture.png';
 import trashIcon from '@assets/images/trash.svg';
 
 import styles from './styles.module.scss';
 
-const CarCard: FC = () => {
+interface CarCardProps {
+  setItemToRemove: Dispatch<(state: string[]) => string[]>;
+  data: {
+    id: string;
+    brandName: string;
+    complectationName: string;
+    modelId: string;
+    modelName: string;
+    photos: string[] | [];
+    position: number;
+    priceEnd: number;
+    priceStart: number;
+    wishlistId: string | undefined;
+  };
+}
+
+const CarCard: FC<CarCardProps> = ({ data, setItemToRemove }) => {
+  const {
+    id,
+    brandName,
+    complectationName,
+    modelName,
+    priceEnd,
+    priceStart,
+    photos,
+  } = data;
+  const title = `${brandName} ${modelName} ${complectationName}`;
+
+  const handleRemove = (): void => {
+    setItemToRemove((state: string[]): string[] => state.concat(id));
+  };
+
   return (
     <div className={styles.card}>
-      <img className={styles.cardImg} src={carImg} alt="car image" />
+      <img className={styles.cardImg} src={photos[0]} alt="car image" />
       <div className={styles.cardContent}>
-        <p className={styles.cardTitle}>BMW X5 M Packet 40d 2015</p>
-        <p className={styles.cardPrice}>$ 34 000 - 52 450</p>
+        <p className={styles.cardTitle}>{title}</p>
+        <p className={styles.cardPrice}>{`$ ${priceStart} - ${priceEnd}`}</p>
       </div>
-      <span className={styles.cardRemove}>
+      <span onClick={handleRemove} className={styles.cardRemove}>
         <img src={trashIcon} alt="delete icon" />
       </span>
     </div>

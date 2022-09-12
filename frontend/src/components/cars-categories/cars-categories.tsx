9fe3f7ee928848e2683ca79a1dfdac86a2954aae
brovarenko <brovwg@gmail.com>
2022-09-12 20:39:45 +0300

    refactoring cars categories

--- frontend/src/components/cars-categories/cars-categories.tsx
+++ frontend/src/components/cars-categories/cars-categories.tsx
@@ -1,16 +1,9 @@
-import React, { FC, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import { SliderNavButton } from '@components/cars-categories/slider-nav-button/slider-nav-button';
 import { swiperParams } from '@components/cars-categories/swiper-params';
-import { useAppSelector, useAppDispatch } from '@hooks/hooks';
-import { setBrandDetailsValue } from '@store/car-filter/slice';
-import { setCars } from '@store/found-car/slice';
-import { API } from '@store/queries/api-routes';
-import {
-  useLazyGetFilteredCarsQuery,
-  useGetBrandsQuery,
-} from '@store/queries/cars';
+import { useGetBrandsQuery } from '@store/queries/cars';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -19,30 +12,7 @@ import 'swiper/css/mousewheel';
 import styles from './styles.module.scss';
 
 const CarsCategories: FC = () => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const { brandDetails } = useAppSelector((state) => state.carFilter);
-  const { id: detailId } = brandDetails[0];
   const { data: brands } = useGetBrandsQuery();
-  const [search, filteredCars] = useLazyGetFilteredCarsQuery();
-
-  useEffect(() => {
-    if (filteredCars.data) {
-      dispatch(setCars(filteredCars.data));
-    }
-  }, [filteredCars]);
-
-  const doSearch = async (queryParams: string[][]): Promise<void> => {
-    dispatch(
-      setBrandDetailsValue({
-        id: detailId,
-        brandId: queryParams[0][1] || '',
-        modelIds: [],
-      }),
-    );
-    await search(queryParams);
-    navigate(API.SEARCH);
-  };
 
   return (
     <div className={styles.container}>
@@ -52,11 +22,8 @@ const CarsCategories: FC = () => {
           <SliderNavButton direction="next" />
 
           {brands?.map((brand) => (
-            <SwiperSlide
-              key={brand.id}
-              onClick={(): Promise<void> => doSearch([['brandId', brand.id]])}
-            >
-              <Link to="#" className={styles.navLink}>
+            <SwiperSlide key={brand.id}>
+              <Link to={`/search?brand=${brand.id}`} className={styles.navLink}>
                 <img
                   className={styles.logo}
                   src={brand.logo_url}

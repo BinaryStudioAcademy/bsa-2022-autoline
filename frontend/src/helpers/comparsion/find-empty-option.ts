1dd8aae060c6929c24b77d8f831439e77f60aaaf
R0ixy <nikita6kom@gmail.com>
2022-09-13 18:25:27 +0300

    modify empty options filter to work with toggle

--- frontend/src/helpers/comparsion/find-empty-option.ts
+++ frontend/src/helpers/comparsion/find-empty-option.ts
@@ -2,10 +2,16 @@ import { ComparisonGeneralInform } from '@autoline/shared';
 
 const findEmptyOptions = (
   generalInfo: ComparisonGeneralInform[] | undefined,
+  toggle: boolean,
+  carOmitOptions: Set<string>,
 ): string[] | undefined => {
   const emptyOptions = generalInfo?.map((e) =>
     Object.entries(e.options)
-      .filter((option) => option[1].length == 0)
+      .filter(
+        (option) =>
+          option[1].filter((option) => !toggle || !carOmitOptions.has(option))
+            .length == 0,
+      )
       .map((option) => option[0]),
   );
   const emptyOptionsForAll = emptyOptions?.reduce((accumulator, currentValue) =>

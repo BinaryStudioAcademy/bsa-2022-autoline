ea70ab273ee202bef4ef2321b5744d76b33c8350
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-09-13 17:42:41 +0300

    create format helper for change requests data

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -1,3 +1,4 @@
 export { getFormValidationResolver } from './validation/validation';
 export { formatPrice } from './utils/format-price';
 export { openOAuthPage } from './sign/open-oauth';
+export { formatComparisonShortData } from './comparison/comparison.helper';

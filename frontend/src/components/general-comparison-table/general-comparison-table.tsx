2109f2b695f189130b5ecc485c3e5b8754e64459
Iryna Burianovata <iryna.burianovata@gmail.com>
2022-09-13 19:10:43 +0300

    Simplify Equal height logic

--- frontend/src/components/general-comparison-table/general-comparison-table.tsx
+++ frontend/src/components/general-comparison-table/general-comparison-table.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo } from 'react';
+import { EqualHeight, EqualHeightElement } from 'react-equal-height';
 import { ScrollSyncPane } from 'react-scroll-sync';
 
 import { CollapseElement } from '@components/collapse-component/collapse-element/collapse-element';
 import { Spinner } from '@components/common/spinner/spinner';
-import { getElementHeightWithMargins } from '@helpers/utils/get-element-height-with-margins';
 import { uuid4 } from '@sentry/utils';
 import { useGetComparisonGeneralInfoQuery } from '@store/queries/comparisons';
 import { clsx } from 'clsx';
@@ -27,10 +27,6 @@ const GeneralComparisonTable: React.FC = () => {
     return options;
   }, [generalInfo]);
 
-  const [generalTable, setGeneralTableRef] = useState<HTMLDivElement | null>(
-    null,
-  );
-
   const broadcast = new BroadcastChannel('compare');
   useEffect(() => {
     broadcast.onmessage = (): void => {
@@ -38,132 +34,103 @@ const GeneralComparisonTable: React.FC = () => {
     };
   }, [broadcast, refetch]);
 
-  useLayoutEffect(() => {
-    if (!generalTable) return;
-
-    //Get all options table cells
-    const optionsValuesElements = generalTable.querySelectorAll(
-      '[data-optionvalue]',
-    ) as NodeListOf<HTMLDivElement>;
-    const optionsValues = [...optionsValuesElements];
-
-    //Create list of heights for all options
-    const optionsHighestHeights = new Map();
-
-    //Go through all options cells to find the larger height of cell
-    optionsValues.forEach((value: HTMLDivElement) => {
-      const borderHeight = 1;
-      //Get height and option title of current table cell
-      const height = getElementHeightWithMargins(value) + borderHeight;
-      const title = value.getAttribute('data-optionvalue');
-
-      //If current table cell has larger height then similar cell of other car, accept it as highest height
-      if (
-        height > optionsHighestHeights.get(title) ||
-        !optionsHighestHeights.has(title)
-      ) {
-        optionsHighestHeights.set(title, height);
-      }
-    });
-
-    //Go through all options cells to set heights of cells
-    optionsValues.forEach((value: HTMLDivElement) => {
-      const title = value.getAttribute('data-optionvalue');
-      //Get title cell of the option, e.g. `Security`
-      const tableTitleElement = generalTable.querySelector(
-        `[data-optiontitle=${title}]`,
-      ) as HTMLDivElement;
-
-      //Set the same height for similar options of cars and their titles
-      //e.g. `Security` cells for all cars have height 100px
-      value.style.height = `${optionsHighestHeights.get(title)}px`;
-      tableTitleElement.style.height = `${optionsHighestHeights.get(title)}px`;
-    });
-  }, [generalTable, generalInfo]);
-
   if (isLoading) return <Spinner />;
 
   return (
     <CollapseElement label="General information" isOpen={true}>
-      <div className={styles.table} ref={setGeneralTableRef}>
-        <div className={clsx(styles.tableTitles, styles.tableColumn)}>
-          <div className={styles.tableCell} data-optiontitle="bodytype">
-            Type
-          </div>
-          <div className={styles.tableCell} data-optiontitle="motor">
-            Motor
-          </div>
-          <div className={styles.tableCell} data-optiontitle="enginepower">
-            Engine Power
-          </div>
-          <div className={styles.tableCell} data-optiontitle="engine">
-            Engine
-          </div>
-          <div className={styles.tableCell} data-optiontitle="wheeldrive">
-            Wheel Drive
-          </div>
-          {[...options].map((option) => (
-            <div
-              className={styles.tableCell}
-              data-optiontitle={option}
-              key={uuid4()}
-            >
-              {option}
-            </div>
-          ))}
-          <div className={styles.tableCell}>Color</div>
-        </div>
-        <ScrollSyncPane>
-          <div className={clsx('styledScrollbar', styles.generalInfo)}>
-            {generalInfo?.map((info) => {
-              return (
+      <EqualHeight>
+        <div className={styles.table}>
+          <div className={clsx(styles.tableTitles, styles.tableColumn)}>
+            <EqualHeightElement name="bodytype">
+              <div className={styles.tableCell}>Type</div>
+            </EqualHeightElement>
+            <EqualHeightElement name="motor">
+              <div className={styles.tableCell}>Motor</div>
+            </EqualHeightElement>
+            <EqualHeightElement name="enginepower">
+              <div className={styles.tableCell}>Engine Power</div>
+            </EqualHeightElement>
+            <EqualHeightElement name="engine">
+              <div className={styles.tableCell}>Engine</div>
+            </EqualHeightElement>
+            <EqualHeightElement name="wheeldrive">
+              <div className={styles.tableCell}>Wheel Drive</div>
+            </EqualHeightElement>
+            {[...options].map((option) => (
+              <EqualHeightElement name={option}>
                 <div
-                  className={clsx(styles.tableData, styles.tableColumn)}
-                  key={info.id}
+                  className={styles.tableCell}
+                  data-optiontitle={option}
+                  key={uuid4()}
                 >
-                  <div className={styles.tableCell} data-optionvalue="bodytype">
-                    {info.bodyType}
-                  </div>
-                  <div className={styles.tableCell} data-optionvalue="motor">
-                    {info.engineDisplacement} l.
-                  </div>
-                  <div
-                    className={styles.tableCell}
-                    data-optionvalue="enginepower"
-                  >
-                    {info.enginePower} h.p.
-                  </div>
-                  <div className={styles.tableCell} data-optionvalue="engine">
-                    {info.engine}
-                  </div>
-                  <div
-                    className={styles.tableCell}
-                    data-optionvalue="wheeldrive"
-                  >
-                    {info.drivetrainName}
-                  </div>
-                  {Object.keys(info.options).map((type: string) => (
-                    <div
-                      className={styles.tableCell}
-                      data-optionvalue={type}
-                      key={uuid4()}
-                    >
-                      {info.options[type].join(', ')}
-                    </div>
-                  ))}
-                  <div className={clsx(styles.tableCell, styles.colorCell)}>
-                    <div
-                      className={styles.colorBox}
-                      style={{ backgroundColor: info.colorName }}
-                    />
-                    <span>{info.colorName}</span>
-                  </div>
+                  {option}
                 </div>
-              );
-            })}
+              </EqualHeightElement>
+            ))}
+            <EqualHeightElement name="color">
+              <div className={clsx(styles.tableCell, styles.lastCell)}>
+                Color
+              </div>
+            </EqualHeightElement>
           </div>
-        </ScrollSyncPane>
-      </div>
+          <ScrollSyncPane>
+            <div className={clsx('styledScrollbar', styles.generalInfo)}>
+              {generalInfo?.map((info) => {
+                return (
+                  <div
+                    className={clsx(styles.tableData, styles.tableColumn)}
+                    key={info.id}
+                  >
+                    <EqualHeightElement name="bodytype">
+                      <div className={styles.tableCell}>{info.bodyType}</div>
+                    </EqualHeightElement>
+                    <EqualHeightElement name="motor">
+                      <div className={styles.tableCell}>
+                        {info.engineDisplacement} l.
+                      </div>
+                    </EqualHeightElement>
+                    <EqualHeightElement name="enginepower">
+                      <div className={styles.tableCell}>
+                        {info.enginePower} h.p.
+                      </div>
+                    </EqualHeightElement>
+                    <EqualHeightElement name="engine">
+                      <div className={styles.tableCell}>{info.engine}</div>
+                    </EqualHeightElement>
+                    <EqualHeightElement name="wheeldrive">
+                      <div className={styles.tableCell}>
+                        {info.drivetrainName}
+                      </div>
+                    </EqualHeightElement>
+                    {Object.keys(info.options).map((type: string) => (
+                      <EqualHeightElement name={type} key={uuid4()}>
+                        <div className={styles.tableCell}>
+                          {info.options[type].join(', ')}
+                        </div>
+                      </EqualHeightElement>
+                    ))}
+                    <EqualHeightElement name="color">
+                      <div
+                        className={clsx(
+                          styles.tableCell,
+                          styles.colorCell,
+                          styles.lastCell,
+                        )}
+                      >
+                        <div
+                          className={styles.colorBox}
+                          style={{ backgroundColor: info.colorName }}
+                        />
+                        <span>{info.colorName}</span>
+                      </div>
+                    </EqualHeightElement>
+                  </div>
+                );
+              })}
+            </div>
+          </ScrollSyncPane>
+        </div>
+      </EqualHeight>
     </CollapseElement>
   );
 };

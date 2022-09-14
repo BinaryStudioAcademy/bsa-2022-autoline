05f3d644be5c395ed8427f4fd71c3ddf6c87b9cf
Andrii Ilnitskyi <elador97@gmail.com>
2022-09-14 15:20:16 +0300

    Optimized max count of brands to show

--- frontend/src/components/cars-categories/cars-categories.tsx
+++ frontend/src/components/cars-categories/cars-categories.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 import { SliderNavButton } from '@components/cars-categories/slider-nav-button/slider-nav-button';
 import { swiperParams } from '@components/cars-categories/swiper-params';
+import { Spinner } from '@components/common/spinner/spinner';
 import { useGetBrandsQuery } from '@store/queries/cars';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -14,10 +15,46 @@ import styles from './styles.module.scss';
 const CarsCategories: FC = () => {
   const { data: brands } = useGetBrandsQuery();
 
+  const countOrAllExisted = useCallback(
+    (count: number): number => {
+      if (!brands) return count;
+      return brands?.length >= count ? count : brands?.length;
+    },
+    [brands],
+  );
+
+  const breakpoints = useMemo(
+    () =>
+      brands && {
+        320: {
+          slidesPerView: countOrAllExisted(3),
+        },
+        640: {
+          slidesPerView: countOrAllExisted(4),
+        },
+        820: {
+          slidesPerView: countOrAllExisted(5),
+        },
+        1080: {
+          slidesPerView: countOrAllExisted(6),
+        },
+        1300: {
+          slidesPerView: countOrAllExisted(8),
+        },
+      },
+    [brands],
+  );
+
+  if (!brands) return <Spinner />;
+
   return (
     <div className={styles.container}>
       <div className={styles.arrowWrapper}>
-        <Swiper className={styles.swiperWrapper} {...swiperParams}>
+        <Swiper
+          className={styles.swiperWrapper}
+          {...swiperParams}
+          breakpoints={breakpoints}
+        >
           <SliderNavButton direction="prev" />
           <SliderNavButton direction="next" />
 
--- frontend/src/components/cars-categories/swiper-params.ts
+++ frontend/src/components/cars-categories/swiper-params.ts
@@ -4,21 +4,4 @@ export const swiperParams: SwiperOptions = {
   loop: true,
   mousewheel: true,
   modules: [Navigation, Mousewheel],
-  breakpoints: {
-    320: {
-      slidesPerView: 3,
-    },
-    640: {
-      slidesPerView: 4,
-    },
-    820: {
-      slidesPerView: 5,
-    },
-    1080: {
-      slidesPerView: 6,
-    },
-    1300: {
-      slidesPerView: 8,
-    },
-  },
 };

20743368754cc6a94d5abe88077e900ad63fd266
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-09-14 23:47:21 +0300

    added message when data is empty

--- frontend/src/components/history-of-comparisons/components/comparisons-grid/comparisons-grid.tsx
+++ frontend/src/components/history-of-comparisons/components/comparisons-grid/comparisons-grid.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react';
 
-import { useGetComparisonCarsQuery } from '@store/queries/comparisons';
+import { Spinner } from '@components/common/spinner/spinner';
+import { useGetAllComparisonsQuery } from '@store/queries/history-of-comparisons';
 
 import { ComparisonsCard } from '../comparisons-card/comparisons-card';
 import styles from './style.module.scss';
 
 const ComparisonsGrid: FC = () => {
-  const { data, isLoading } = useGetComparisonCarsQuery();
+  const { data, isLoading } = useGetAllComparisonsQuery();
 
-  if (isLoading) return null;
+  if (isLoading) return <Spinner />;
+
+  if (data?.count === 0) return <p>Nothing in the list of comparisons cars.</p>;
 
   return (
     <div className={styles.carGrid}>
       {data &&
-        [data, data, data].map((item) => <ComparisonsCard data={item} />)}
+        data.comparisons.map((item) => (
+          <ComparisonsCard data={item} key={item.id} />
+        ))}
     </div>
   );
 };

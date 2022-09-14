fa05ef46df72c2f6ed275cdf04bd6c2706d7e137
Andrii Ilnitskyi <elador97@gmail.com>
2022-09-15 00:43:06 +0300

    fixed old search results when moving from landing

--- frontend/src/components/search-page/search-page.tsx
+++ frontend/src/components/search-page/search-page.tsx
@@ -2,18 +2,27 @@ import { AdvancedAutoFilter } from '@components/advanced-auto-filter/advanced-au
 import { AppliedFiltersBar } from '@components/applied-filters-bar/applied-filters-bar';
 import { CarListItem } from '@components/car-list-item/car-list-item';
 import { PageContainer } from '@components/common/page-container/page-container';
+import { Spinner } from '@components/common/spinner/spinner';
 import { Title } from '@components/common/title/title';
 import { Header } from '@components/header/header';
 import { TopCarsAutoria } from '@components/top-cars-autoria/top-cars-autoria';
 import { useAppSelector } from '@hooks/hooks';
 import SearchIcon from '@mui/icons-material/Search';
 import { Grid } from '@mui/material';
+import { carsApi } from '@store/queries/cars';
+import { selectFiltersQueryArr } from '@store/selectors/car-filter-selectors';
 import { clsx } from 'clsx';
 
 import styles from './styles.module.scss';
 
 const SearchPage: React.FC = () => {
   const cars = useAppSelector((state) => state.foundCars.cars);
+
+  const filtersQueryArr = useAppSelector(selectFiltersQueryArr);
+
+  const { isLoading } = useAppSelector((state) =>
+    carsApi.endpoints.getFilteredCars.select(filtersQueryArr)(state),
+  );
 
   return (
     <>
@@ -25,7 +34,8 @@ const SearchPage: React.FC = () => {
         <Grid container spacing={2} className={styles.searchPageContent}>
           <Grid item xs={12} md={8}>
             <AppliedFiltersBar />
-            {cars.length === 0 && (
+            {isLoading && <Spinner />}
+            {cars.length === 0 && !isLoading && (
               <>
                 <div className={styles.nothingFound}>
                   <SearchIcon
@@ -38,9 +48,8 @@ const SearchPage: React.FC = () => {
                 />
               </>
             )}
-            {cars.map((car) => (
-              <CarListItem {...car} key={car.model_id} />
-            ))}
+            {!isLoading &&
+              cars.map((car) => <CarListItem {...car} key={car.model_id} />)}
           </Grid>
           <Grid item xs={12} md={4}>
             <AdvancedAutoFilter />

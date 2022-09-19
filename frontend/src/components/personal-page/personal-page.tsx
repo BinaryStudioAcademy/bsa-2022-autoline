add90260fa7749397fa710a3749a5d8f5d3581bf
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-09-19 10:47:48 +0300

    added query and spinner on the personal page

--- frontend/src/components/personal-page/personal-page.tsx
+++ frontend/src/components/personal-page/personal-page.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import { PageContainer } from '@components/common/page-container/page-container';
+import { Spinner } from '@components/common/spinner/spinner';
 import { Title } from '@components/common/title/title';
 import { Header } from '@components/header/header';
 import { HistoryOfComparisons } from '@components/history-of-comparisons/history-of-comparisons';
@@ -6,8 +9,20 @@ import { UserInfoPanel } from '@components/user-info-panel/user-info-panel';
 import { ViewedCars } from '@components/viewed-cars/viewed-cars';
 import { Wishlist } from '@components/wishlist/wishlist';
 import { Grid } from '@mui/material';
+import { useGetHistoryOfViwedCarsQuery } from '@store/queries/history-viewed-cars';
 
 const PersonalPage = (): JSX.Element => {
+  const [params] = useState({
+    skip: '0',
+    take: '10',
+  });
+
+  const { isLoading } = useGetHistoryOfViwedCarsQuery(params);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <Header />

547aaa2194180a5267191c08e426da49c5c41a8c
brovarenko <brovwg@gmail.com>
2022-09-21 11:42:12 +0300

    fixed link scroll

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -6,14 +6,25 @@ import { AppRoute } from '@common/enums/enums';
 import styles from './styles.module.scss';
 
 export const Footer = (): React.ReactElement => {
+  const scroll = (): void => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className={styles.footer}>
       <div className={styles.links}>
-        <Link to={AppRoute.ROOT}>
+        <Link to={AppRoute.ROOT} onClick={scroll}>
           <img className={styles.logo} src={Logo} alt="Autoline" />
         </Link>
-        <Link to={AppRoute.SEARCH}>Search</Link>
-        <Link to={AppRoute.ABOUT}>About us</Link>
+        <Link to={AppRoute.SEARCH} onClick={scroll}>
+          Search
+        </Link>
+        <Link to={AppRoute.ABOUT} onClick={scroll}>
+          About us
+        </Link>
       </div>
 
       <div className={styles.copyright}>Copyright © Autoline 2022</div>
